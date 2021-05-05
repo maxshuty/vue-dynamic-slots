21a51a5aafa4dc42aee1f17f5a9c71021410f8b8
Max Poshusta <MaxPoshusta@gmail.com>
2021-05-05 06:11:36 -0500

    Adding App.vue

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <slots-example>
+            <!--'companyName', 'addressLine1', 'city', 'state', 'country', 'postalCode'-->
+            <template v-slot:companyName>
+                <div><b>Company Name:</b> Elves LLC</div>
+            </template>
+            <template v-slot:addressLine1>
+                <div><b>Address Line 1:</b> 123 Road NorthPole</div>
+            </template>
+            <template v-slot:city>
+                <div><b>City:</b> North Pole</div>
+            </template>
+            <template v-slot:state>
+                <div><b>State:</b> Alaska</div>
+            </template>
+            <template v-slot:country>
+                <div><b>Country:</b> United States</div>
+            </template>
+            <template v-slot:postalCode>
+                <div><b>Postal Code:</b> 12345</div>
+            </template>
+        </slots-example>
+    </div>
+</template>
+
+<script>
+import SlotsExample from './components/SlotsExample.vue';
+
+export default {
+    components: {SlotsExample},
+    name: 'App',
+};
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
